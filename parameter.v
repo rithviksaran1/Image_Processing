b6c085393599422db65b527a563ce5736c65d6ed
rithviksaran1 <rithvik167@gmail.com>
2025-07-08 21:56:45 +0530

    Create parameter.v

---
+++ parameter.v
@@ -0,0 +1,9 @@
+`define INPUTFILENAME		  // Enter Input file name in " " - hex format obtained from Matlab operation
+`define OUTPUTFILENAME		// Enter Output file name in " " - under which the processed image is stored
+
+// Choose the operation of code by removing comment lines for specific operation
+
+`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+//`define BLACKandWHITE_OPERATION
+//`define THRESHOLD_OPERATION 
